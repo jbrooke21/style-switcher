ec166da3573cc825b95d2bebb5ae0deeeeb5eb15
jbrooke21 <jessica.brooke@symphony.com>
2016-05-03 18:13:52 -0700

    style switcher

---
+++ style-switcher/app.js
@@ -0,0 +1,47 @@
+
+
+$(document).ready(function() {
+
+    $(document).on("click", "#change-red", function() {
+    	$("#red-div")
+    		.css ("background-color", "red");
+    });
+
+    $(document).on("click", "#change-green", function() {
+		$("#green-div")
+			.css("background-color", "green");
+	});
+
+	$(document).on("click", "#change-blue", function() {
+		$("#blue-div")
+			.css("background-color", "blue");
+	});
+
+	$(document).on("click", "#make-big", function() {
+    	$("#big-div").toggleClass("big-red");
+    		
+    });
+
+	$(document).on("click", "#make-small", function() {
+    	$("#small-div").toggleClass("small-green");
+    		
+    });
+
+    $(document).on("click", "#animate-div", function() {
+    	$("#change-div").toggleClass("animation");
+    		
+    });
+
+   	$(document).on("click", "#create-overlay", function() {
+   		$(".overlay").show("slow");
+   	});
+
+    $(document).on("click", ".overlay", function() {
+      $(this).hide("slow");
+    });
+
+
+
+
+});
+
